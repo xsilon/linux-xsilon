4a01209ef4bd198712ae1b234aacb8684dcd417e
Simon Vincent <simon.vincent@xsilon.com>
2015-01-05 14:32:28 +0000

    SPI bus_num leave at -1 so that spi.c can select an adaquate number

--- drivers/spi/spi-xilinx.c
+++ drivers/spi/spi-xilinx.c
@@ -369,7 +369,6 @@ static int xilinx_spi_probe(struct platform_device *pdev)
 		goto put_master;
 	}
 
-	master->bus_num = pdev->dev.id;
 	master->num_chipselect = num_cs;
 	master->dev.of_node = pdev->dev.of_node;
 
