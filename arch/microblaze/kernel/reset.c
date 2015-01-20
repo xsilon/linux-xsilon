75e4171ff9d886f5499230e0af366fb12e2d4c57
Simon Vincent <simon.vincent@xsilon.com>
2015-01-20 12:51:42 +0000

    Bug 130: Hacked microblaze reset driver to use our fpga_config register. Linux can now be rebooted using reboot cmd.

--- arch/microblaze/kernel/reset.c
+++ arch/microblaze/kernel/reset.c
@@ -11,72 +11,23 @@
 #include <linux/of_platform.h>
 #include <asm/prom.h>
 
-/* Trigger specific functions */
-#ifdef CONFIG_GPIOLIB
-
-#include <linux/of_gpio.h>
-
-static int handle; /* reset pin handle */
-static unsigned int reset_val;
-
-void of_platform_reset_gpio_probe(void)
-{
-	int ret;
-	handle = of_get_named_gpio(of_find_node_by_path("/"),
-				   "hard-reset-gpios", 0);
-
-	if (!gpio_is_valid(handle)) {
-		pr_info("Skipping unavailable RESET gpio %d (%s)\n",
-				handle, "reset");
-		return;
-	}
-
-	ret = gpio_request(handle, "reset");
-	if (ret < 0) {
-		pr_info("GPIO pin is already allocated\n");
-		return;
-	}
-
-	/* get current setup value */
-	reset_val = gpio_get_value(handle);
-	/* FIXME maybe worth to perform any action */
-	pr_debug("Reset: Gpio output state: 0x%x\n", reset_val);
-
-	/* Setup GPIO as output */
-	ret = gpio_direction_output(handle, 0);
-	if (ret < 0)
-		goto err;
-
-	/* Setup output direction */
-	gpio_set_value(handle, 0);
-
-	pr_info("RESET: Registered gpio device: %d, current val: %d\n",
-							handle, reset_val);
-	return;
-err:
-	gpio_free(handle);
-	return;
-}
+#include <linux/io.h>
 
 
 static void gpio_system_reset(void)
 {
-	if (gpio_is_valid(handle))
-		gpio_set_value(handle, 1 - reset_val);
-	else
-		pr_notice("Reset GPIO unavailable - halting!\n");
-}
-#else
-static void gpio_system_reset(void)
-{
-	pr_notice("No reset GPIO present - halting!\n");
+	/* Quick hack to reset system */
+	void *reg;
+	reg= ioremap_nocache(0x70800028, 4096);
+	*((unsigned long *)reg)=0x0; 
 }
 
 void of_platform_reset_gpio_probe(void)
 {
+	/* Don't do anything here. */
 	return;
 }
-#endif
+
 
 void machine_restart(char *cmd)
 {
